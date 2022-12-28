5b76419b5f74dab5fcdf7d89e76135d9d2bd65be
ziontee113 <ziontee113@gmail.com>
2022-12-29 06:58:51 +0700

    Handle remove sequence element on key up

--- src/event_processor/mod.rs
+++ src/event_processor/mod.rs
@@ -1,6 +1,8 @@
 use crate::stuffs::{key_state::KeyState, keyboard_event::KeyboardEvent};
 
+#[derive(Getters)]
 struct SequenceManager<'a> {
+    #[getset(get = "pub")]
     sequence: Vec<KeyboardEvent<'a>>,
 }
 
@@ -15,7 +17,9 @@ impl<'a> SequenceManager<'a> {
                 self.sequence.push(event);
             }
             KeyState::Up => {
-                todo!()
+                if event.key() == self.sequence.last().unwrap().key() {
+                    self.sequence.pop();
+                }
             }
             KeyState::Hold => (),
         }
@@ -34,20 +38,28 @@ mod sequence_manager_module_test {
         let L1 = Keyboard::new("L1", "My Left Keyboard", "usb/0/0/input0");
         let R1 = Keyboard::new("R1", "My Right Keyboard", "usb/1/1/input0");
 
-        let mut sequence_manager = SequenceManager::new();
+        let mut manager = SequenceManager::new();
 
         let event = tke!(L1 LEFTCTRL Down 0);
-        sequence_manager.receive(event);
+        manager.receive(event);
         assert_eq!(
-            *sequence_manager.sequence.first().unwrap(),
+            *manager.sequence().get(0).unwrap(),
             tke!(L1 LEFTCTRL Down 0)
         );
 
         let event = tke!(R1 J Down 50);
-        sequence_manager.receive(event);
-        assert_eq!(
-            *sequence_manager.sequence.get(1).unwrap(),
-            tke!(R1 J Down 50)
-        );
+        manager.receive(event);
+        assert_eq!(*manager.sequence().get(1).unwrap(), tke!(R1 J Down 50));
+    }
+
+    #[test]
+    fn can_remove_sequence_element_on_key_up() {
+        let L1 = Keyboard::new("L1", "My Left Keyboard", "usb/0/0/input0");
+        let mut manager = SequenceManager::new();
+
+        manager.receive(tke!(L1 LEFTCTRL Down 0));
+        manager.receive(tke!(L1 J Down 50));
+        manager.receive(tke!(L1 J Up 100));
+        assert_eq!(manager.sequence().len(), 1);
     }
 }
